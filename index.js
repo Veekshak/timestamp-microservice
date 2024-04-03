b08e68a81433ea1cb2dd57757162f103293327f9
Veekshak <veekshuk9009@gmail.com>
2024-04-03 12:05:58 +0530

    First Project for freecodecamp

--- index.js
+++ index.js
@@ -2,31 +2,63 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.route("/api/:date").get((req, res) => {
+  const data = req.params.date;
+
+  try {
+    if (isNaN(data)) {
+      const date = new Date(data);
+      if (date == "Invalid Date")
+        return res.status(400).send({ message: "Invalid Date" });
+      else ifString(date);
+    } else {
+      const millisecond = parseInt(data);
+      const date = new Date(millisecond);
+      ifMillisecond(date);
+    }
+  } catch (error) {
+    return res.status(400).send({ message: error });
+  }
+
+  function toUTCTimeStamp(value) {
+    return value.toUTCString();
+  }
+
+  function ifString(date) {
+    const unixTimeStamp = date.getTime();
+    const utcTimeStamp = toUTCTimeStamp(date);
+
+    res.send({ unix: unixTimeStamp, utc: utcTimeStamp });
+  }
+
+  function ifMillisecond(date) {
+    const utcTimeStamp = toUTCTimeStamp(date);
+    res.send({ unix: data, utc: utcTimeStamp });
+  }
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
